00c0417116c41fdf4a949ada73f724f6be8a7bcb
Shalisska <alena.simonova83@gmail.com>
2019-08-31 20:00:22 +0300

    Add project files.

---
+++ DBMigrations/Migrations/20190831190900_AddFieldsTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentMigrator;
+
+namespace DBMigrations.Migrations
+{
+    [Migration(20190831190900)]
+    public class AddFieldsTable : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Fields")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Culture").AsString()
+                .WithColumn("Ready").AsDateTime()
+                .WithColumn("Grazing").AsDateTime()
+                .WithColumn("Fertilizing").AsDateTime()
+                .WithColumn("Sowing").AsDateTime()
+                .WithColumn("Growing").AsDateTime()
+                .WithColumn("Harvesting").AsDateTime()
+                .WithColumn("Restoring").AsDateTime();
+        }
+    }
+}
---
+++ DBMigrations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DBMigrations.Migrations;
+
+using FluentMigrator.Runner;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DBMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\projects\\MyClonesData\\MyClones.mdf;Integrated Security=True;Connect Timeout=30")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(AddFieldsTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
