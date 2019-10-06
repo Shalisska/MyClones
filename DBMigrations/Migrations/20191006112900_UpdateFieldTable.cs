9e6e7536ef00269da38cb79d7862234e8192a6b2
Shalisska <alena.simonova83@gmail.com>
2019-10-06 12:34:39 +0300

    update field table

---
+++ DBMigrations/Migrations/20191006112900_UpdateFieldTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace DBMigrations.Migrations
+{
+    [Migration(20191006112900)]
+    public class UpdateFieldTable : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Fields")
+                .AlterColumn("Culture").AsString().Nullable()
+                .AddColumn("HouseLocation").AsString()
+                .AddColumn("CultureSeedPrice").AsDecimal(18, 4)
+                .AddColumn("FertilizePrice").AsDecimal(18, 4)
+                .AddColumn("HarvestTax").AsDecimal(18, 4);
+        }
+    }
+}
--- DBMigrations/Program.cs
+++ DBMigrations/Program.cs
@@ -31,10 +31,10 @@ namespace DBMigrations
                     // Add SQLite support to FluentMigrator
                     .AddSqlServer()
                     // Set the connection string
-                    //.WithGlobalConnectionString("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\projects\\MyClonesData\\MyClones.mdf;Integrated Security=True;Connect Timeout=30")
-                    .WithGlobalConnectionString("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=MyClones;Integrated Security=True;")
+                    .WithGlobalConnectionString("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\projects\\MyClonesData\\MyClones.mdf;Integrated Security=True;Connect Timeout=30")
+                    //.WithGlobalConnectionString("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=MyClones;Integrated Security=True;")
                     // Define the assembly containing the migrations
-                    .ScanIn(typeof(AddFieldsTable).Assembly).For.Migrations())
+                    .ScanIn(typeof(UpdateFieldTable).Assembly).For.Migrations())
                 // Enable logging to console in the FluentMigrator way
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
                 // Build the service provider
